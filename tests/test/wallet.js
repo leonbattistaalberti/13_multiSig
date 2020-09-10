5dc16c8193dcaf3713547046936403b3c24c7613
thelonehegelian <thelonehegelian@gmail.com>
2020-09-10 16:42:37 +0100

    Passed createTransaction

--- tests/test/wallet.js
+++ tests/test/wallet.js
@@ -1,8 +1,16 @@
-const Wallet = artifacts.require('Wallet');
+const Wallet = artifacts.require("Wallet");
 
-contract('Wallet', (accounts) => {
-  let wallet = null;
-  before(async () => {
-    wallet = await Wallet.deployed();
-  });
+contract("Wallet", (accounts) => {
+	let wallet = null;
+	before(async () => {
+		wallet = await Wallet.deployed();
+	});
+
+	it("Should create a transfer", async () => {
+		let to = accounts[5];
+		approver = accounts[0];
+		await wallet.createTransfer(1000, to, { from: approver });
+		let transfer = await wallet.transfers(0);
+		assert(transfer.id.toNumber() === 0);
+	});
 });
