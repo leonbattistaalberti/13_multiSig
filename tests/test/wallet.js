fd10bd35af01763f962a2cf9e0343f1424d5afbb
thelonehegelian <thelonehegelian@gmail.com>
2020-09-10 22:19:13 +0100

    install openzepplin/testhelpers

--- tests/test/wallet.js
+++ tests/test/wallet.js
@@ -1,3 +1,4 @@
+const { expectRevert } = require("@openzeppelin/test-environment");
 const Wallet = artifacts.require("Wallet");
 
 contract("Wallet", (accounts) => {
@@ -8,9 +9,17 @@ contract("Wallet", (accounts) => {
 
 	it("Should create a transfer", async () => {
 		let to = accounts[5];
-		approver = accounts[0];
+		let approver = accounts[0];
 		await wallet.createTransfer(1000, to, { from: approver });
 		let transfer = await wallet.transfers(0);
 		assert(transfer.id.toNumber() === 0);
+	});
+	it("Should not create transfer if not approver", async () => {
+		let to = accounts[5];
+		let approver = accounts[0];
+		await expectRevert(
+			wallet.createTransfer(1000, to, { from: approver }),
+			"only approver allowed"
+		);
 	});
 });
