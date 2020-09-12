4a32405e1a983d901326cf3cb2c7516ddebd73ad
thelonehegelian <thelonehegelian@gmail.com>
2020-09-12 09:36:06 +0100

    Pass should send transfer if approvers number reached

--- tests/test/wallet.js
+++ tests/test/wallet.js
@@ -2,40 +2,51 @@ const { expectRevert } = require("@openzeppelin/test-helpers");
 const Wallet = artifacts.require("Wallet");
 
 contract("Wallet", (accounts) => {
-	let wallet = null;
-	before(async () => {
-		wallet = await Wallet.deployed();
-	});
+    let wallet = null;
+    before(async () => {
+        wallet = await Wallet.deployed();
+    });
 
-	it("Should create a transfer", async () => {
-		let to = accounts[5];
-		let approver = accounts[0];
-		await wallet.createTransfer(1000, to, { from: approver });
-		let transfer = await wallet.transfers(0);
-		assert(transfer.id.toNumber() === 0);
-	});
-	it("Should not create transfer if not approver", async () => {
-		let to = accounts[5];
-		let approver = accounts[5];
-		await expectRevert(
-			wallet.createTransfer(1000, to, { from: approver }),
-			"only approver allowed"
-		);
-	});
-	it("Should not send transfer if not enough approvers", async () => {
-		/**
-		 *	1. get balance
-		 *	2. createTransfer with a non-approver
-		 *	3. get balance
-		 *	4. compare balance
-		 *	5. assert
-		 */
-		let to = accounts[5];
-		let approver = accounts[0];
-		const initialBalance = web3.utils.toBN(await web3.eth.getBalance(to));
-		await wallet.createTransfer(1000, to, { from: approver });
-		await wallet.sendTransfer(0, { from: approver });
-		const finalBalance = web3.utils.toBN(await web3.eth.getBalance(to));
-		assert(finalBalance.sub(initialBalance).isZero());
-	});
+    it("Should create a transfer", async () => {
+        let to = accounts[5];
+        let approver = accounts[0];
+        await wallet.createTransfer(1000, to, { from: approver });
+        let transfer = await wallet.transfers(0);
+        assert(transfer.id.toNumber() === 0);
+    });
+    it("Should not create transfer if not approver", async () => {
+        let to = accounts[5];
+        let approver = accounts[5];
+        await expectRevert(
+            wallet.createTransfer(1000, to, { from: approver }),
+            "only approver allowed"
+        );
+    });
+    it("Should not send transfer if not enough approvers", async () => {
+        /**
+         *	1. get balance
+         *	2. createTransfer with a non-approver
+         *	3. get balance
+         *	4. compare balance
+         *	5. assert
+         */
+        let to = accounts[5];
+        let approver = accounts[0];
+        const initialBalance = web3.utils.toBN(await web3.eth.getBalance(to));
+        await wallet.createTransfer(1000, to, { from: approver });
+        await wallet.sendTransfer(0, { from: approver });
+        const finalBalance = web3.utils.toBN(await web3.eth.getBalance(to));
+        assert(finalBalance.sub(initialBalance).isZero());
+    });
+
+    it('Should send transfer if approvers number reached', async () => {
+        let to = accounts[5];
+        let approver = accounts[0];
+        const initialBalance = web3.utils.toBN(await web3.eth.getBalance(to));
+        await wallet.createTransfer(1000, to, { from: approver });
+        await wallet.sendTransfer(2, { from: approver });
+        await wallet.sendTransfer(2, { from: accounts[1] });
+        const finalBalance = web3.utils.toBN(await web3.eth.getBalance(to));
+        assert.equal(finalBalance.sub(initialBalance), 1000);
+    })
 });
